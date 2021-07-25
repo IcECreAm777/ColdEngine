5b1c8c3f88138c5af0a3a9fc85023d35ede6ae94
IcECreAm777 <31211782+IcECreAm777@users.noreply.github.com>
2021-07-25 07:35:09 +0200

    holiday commit

---
+++ ColdEngine/Graphic/Shader.cpp
@@ -0,0 +1,21 @@
+﻿#include "Shader.h"
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath)
+{
+}
+
+void Shader::Use()
+{
+}
+
+void Shader::SetBool(const std::string& name, bool value) const
+{
+}
+
+void Shader::SetInt(const std::string& name, int value) const
+{
+}
+
+void Shader::SetFloat(const std::string& name, float value) const
+{
+}
--- ColdEngine/main.cpp
+++ ColdEngine/main.cpp
@@ -3,9 +3,75 @@
 #include <GLFW/glfw3.h>
 
 // Callback function for glfw when the window is resized by the user
-void framebuffer_size_callback(GLFWwindow* window, const int width, const int height)
+void framebufferSizeCallback(GLFWwindow* window, const int width, const int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+//TODO delete
+const char *vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n\0";
+
+//TODO move to a different class
+unsigned int buildAndCompileShader()
+{
+	//set up vertex shader
+	const unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+
+	// check for compile errors for the vertex shader
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+		std::cout << "ERROR: Vertex Shader compilation failed\n" << infoLog << std::endl;
+	}
+
+	// set up fragment shader
+	const unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glCompileShader(fragmentShader);
+
+	// check for compile errors for the fragment shader
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+		std::cout << "ERROR: Fragment Shader compilation failed\n" << infoLog << std::endl;
+	}
+
+	// link shaders
+	const unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if(!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+		std::cout << "ERROR: Linking failed\n" << infoLog << std::endl;
+	}
+
+	// delete shaders after linking
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
 }
 
 int main()
@@ -15,6 +81,10 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
 	// initializes a window and terminates when it couldn't be created
 	GLFWwindow* window = glfwCreateWindow(800, 600, "ColdEngine", nullptr, nullptr);
@@ -38,14 +108,75 @@ int main()
 	glViewport(0, 0, 800, 600);
 
 	// binds the callback to the resize event
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
+	// initialize open GL Data
+	unsigned int shaderProgram = buildAndCompileShader();
+
+	float vertices[] = {
+		-.5f, -.5f, 0.f,	// bottom left
+		.5f, -.5f, 0.f,		// bottom right
+		.5f, .5f, 0.f,		// top right
+		-.5f, .5f, 0.f		// top left
+	};
+
+	unsigned int indices[] = {
+		1, 2, 3,
+		0, 1, 3 
+	};
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0); 
+	
+	double previousFrame = glfwGetTime();
 	// Render Loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// calculate frame rate
+		const double thisFrame = glfwGetTime();
+		const double fps = 1000 / (thisFrame - previousFrame); 
+		previousFrame = thisFrame; //TODO display in profiler
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//TODO input
+
+		//TODO rendering
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	// deallocate used buffers and stuff
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
 	return 0;
